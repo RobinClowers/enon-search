7416e4244fc5634098f638f1ce6573de0f5aea24
Robin Clowers <robin.clowers@gmail.com>
2022-08-20 10:45:56 -0700

    bin/search added

--- search.rb
+++ search.rb
@@ -5,8 +5,16 @@ require './index'
 class Search
   def self.search
     term = ENV.fetch('QUERY_TERM', 'test').downcase
-    result = Search.new.search(term)
+    Search.new.search(term)
+  end
 
+  def initialize(data_path = IndexFile.base_path)
+    @data_path = data_path
+    @index = Index.new(data_path)
+  end
+
+  def search(term)
+    result = @index.search(term)
     unless result.empty?
       puts result.first.match(/^From:.*$/i)
       puts result.first.match(/^To:.*$/i)
@@ -18,15 +26,4 @@ class Search
     puts
     puts "Search term `#{term}` returned #{result.count} documents"
   end
-
-  def initialize(data_path = IndexFile.base_path)
-    @data_path = data_path
-    @index = Index.new(data_path)
-  end
-
-  def search(term)
-    @index.search(term)
-  end
 end
-
-Search.search
